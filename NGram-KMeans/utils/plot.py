0f51af98c68ddb95691006f3b30a8902a90573bd
Silence <daniel.s.mo503@gmail.com>
2018-05-10 01:01:22 +0800

    添加二维散点图绘制

--- NGram-KMeans/utils/plot.py
+++ NGram-KMeans/utils/plot.py
@@ -16,3 +16,16 @@ def plot_3d_scatter(data, labels=[]):
 	ax = fig.gca(projection='3d')
 	ax.scatter(data[:,0],data[:,1], data[:,2], c=labels)
 	plt.show()
+
+# 作出二维散点图，并根据分类结果标出不同颜色
+# 参数格式：
+# 		data：numpy.array，坐标(x,y)的数组
+#		labels：一维数组，表示聚类算法的分类结果
+def plot_scatter(data, labels=[]):
+	# 设置labels参数默认值为全0
+	labels = labels if len(labels) != 0 else [0 for x in range(len(data))]
+
+	fig = plt.figure()
+	ax = fig.gca()
+	ax.scatter(data[:,0],data[:,1], c=labels)
+	plt.show()
