989d46ff0799c1f1f409b3283bfd96be1fa6d876
Silence <daniel.s.mo503@gmail.com>
2018-12-20 16:34:56 +0800

    完成原始数据读取

--- build/elm/vector_generator.py
+++ build/elm/vector_generator.py
@@ -1,6 +1,30 @@
 import io
+import re
 
-with io.open("./temp/ns.tmp", "r", encoding="utf8") as f:
-	ns_dict = eval(f.read())
+def read_dict(path):
+	with io.open(path, "r", encoding="utf8") as f:
+		return eval(f.read())
 
-	print(ns_dict["220.194.200.70"])
+def read_file(path):
+	ret = []
+
+	with io.open(path, "r", encoding="utf8") as f:
+		for line in f.readlines():
+			rs = re.search(r"(.+)(\[.+\])(.+)\:(.+)", line)
+
+			domain = rs.group(1)
+			ips = eval(rs.group(2))
+			times = rs.group(3).split(",")
+			ttl = int(times[0])
+			expire = int(times[3])
+			nss = rs.group(4).split(",")
+
+			ret.append({domain: {"iplist": ips, "nslist": nss, "ttl": ttl, "whois_expire": expire}})
+
+	return ret
+
+
+if __name__ == '__main__':
+	raw = read_file("./temp/data")
+	ns_dict = read_dict("./temp/ns.tmp")
+	resolved_dict = read_dict("./temp/resolved.tmp")
