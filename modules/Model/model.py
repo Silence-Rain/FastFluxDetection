1bbb09b3c5ee4c65f9468acc6f627ced9cc6e1a2
Silence <daniel.s.mo503@gmail.com>
2018-12-20 16:19:03 +0800

    增加model的单元测试

--- modules/Model/model.py
+++ modules/Model/model.py
@@ -9,69 +9,69 @@ from config import *
 
 # 获取域名测度信息的model
 class DetailModel(object):
-	# def __init__(self):
-	# 	self.dns = MySQL(
-	# 		host=DNS_HOST,
-	# 		user=DNS_USER,
-	# 		passwd=DNS_PASSWD,
-	# 		port=DNS_PORT,
-	# 		db=DNS_DB
-	# 		)
-	# 	self.initCppLib()
+	def __init__(self):
+		self.dns = MySQL(
+			host=DNS_HOST,
+			user=DNS_USER,
+			passwd=DNS_PASSWD,
+			port=DNS_PORT,
+			db=DNS_DB
+			)
+		self.initCppLib()
 
-	# # 初始化C++动态库接口
-	# def initCppLib(self):
-	# 	ll = cdll.LoadLibrary
-	# 	lib = ll("../../lib/libPrimaryDomain.so")
-	# 	init = lib.init
-	# 	self.getPrimaryDomain = lib.getPrimaryDomain
-	# 	self.getPrimaryDomain.argtypes = [c_char_p]
-	# 	self.getPrimaryDomain.restype = c_char_p
-	# 	init()
+	# 初始化C++动态库接口
+	def initCppLib(self):
+		ll = cdll.LoadLibrary
+		lib = ll("../../lib/libPrimaryDomain.so")
+		init = lib.init
+		self.getPrimaryDomain = lib.getPrimaryDomain
+		self.getPrimaryDomain.argtypes = [c_char_p]
+		self.getPrimaryDomain.restype = c_char_p
+		init()
 
-	# # 获取域名whois信息
-	# # 参数：全域名
-	# # 返回值：[whois信息]
-	# def get_whois(self, domain):
-	# 	pd = self.getPrimaryDomain(domain.encode("utf-8")).decode()
-	# 	sql = "SELECT * FROM domain_whois WHERE primary_domain='%s';" % pd
-	# 	rs = self.dns.get(sql)
+	# 获取域名whois信息
+	# 参数：全域名
+	# 返回值：[whois信息]
+	def get_whois(self, domain):
+		pd = self.getPrimaryDomain(domain.encode("utf-8")).decode()
+		sql = "SELECT * FROM domain_whois WHERE primary_domain='%s';" % pd
+		rs = self.dns.get(sql)
 
-	# 	if rs != None:
-	# 		return list(rs)
-	# 	else:
-	# 		return []
+		if rs != None:
+			return list(rs)
+		else:
+			return []
 
-	# # 获取域名解析IP地理位置
-	# # 参数：[解析IP（前100个）]
-	# # 返回值：{ip:(国家,地区,城市)...}
-	# def get_ip_location(self, ips):
-	# 	# ret = {}
-	# 	# for ip in ips[:100]:
-	# 	# 	sql = "SELECT country,region,city FROM IP2Location WHERE ipStart<%s AND ipEnd>%s;" % (ip, ip)
-	# 	# 	rs = self.ipcis.get(sql)
-	# 	# 	ret[ip] = rs
+	# 获取域名解析IP地理位置
+	# 参数：[解析IP（前100个）]
+	# 返回值：{ip:(国家,地区,城市)...}
+	def get_ip_location(self, ips):
+		# ret = {}
+		# for ip in ips[:100]:
+		# 	sql = "SELECT country,region,city FROM IP2Location WHERE ipStart<%s AND ipEnd>%s;" % (ip, ip)
+		# 	rs = self.ipcis.get(sql)
+		# 	ret[ip] = rs
 
-	# 	# return ret
+		# return ret
 
-	# 	proxy = {"http": "http://yunyang:yangyun123@202.112.23.167:8080"}
-	# 	cur_date = time.strftime("%Y-%m-%d", time.localtime())
-	# 	ret = {}
-	# 	for ip in ips[:100]:
-	# 		ip_str = str(IPy.IP(ip))
-	# 		# 查询当天的IP活动流记录
-	# 		r = requests.get("http://211.65.197.210:8080/IPCIS/activityDatabase/"
-	# 			"?IpSets=%s:32&TableName=%s&Mode=1" % (ip_str, cur_date), proxies=proxy, timeout=10)
-	# 		res = r.json()[ip_str+":32"]
-	# 		for i in res:
-	# 			# 获得对端IP地理位置
-	# 			for item in i[1:]:
-	# 				if ip not in ret:
-	# 					ret[ip] = [item.split("$")[-2].split("-")]
-	# 				else:
-	# 					ret[ip].append(item.split("$")[-2].split("-"))
+		proxy = {"http": "http://yunyang:yangyun123@202.112.23.167:8080"}
+		cur_date = time.strftime("%Y-%m-%d", time.localtime())
+		ret = {}
+		for ip in ips[:100]:
+			ip_str = str(IPy.IP(ip))
+			# 查询当天的IP活动流记录
+			r = requests.get("http://211.65.197.210:8080/IPCIS/activityDatabase/"
+				"?IpSets=%s:32&TableName=%s&Mode=1" % (ip_str, cur_date), proxies=proxy, timeout=10)
+			res = r.json()[ip_str+":32"]
+			for i in res:
+				# 获得对端IP地理位置
+				for item in i[1:]:
+					if ip not in ret:
+						ret[ip] = [item.split("$")[-2].split("-")]
+					else:
+						ret[ip].append(item.split("$")[-2].split("-"))
 
-	# 	return ret
+		return ret
 
 
 	# 获取域名解析IP经纬度
